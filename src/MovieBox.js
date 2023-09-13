ce9744ba2f278659a215db9a1eb244e70b1c4a1f
D-realsamdev <ajakayedamilola7@gmail.com>
2023-09-13 18:32:51 +0100

    update

--- src/MovieBox.js
+++ src/MovieBox.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import Rating from './assets/Rating.png'
+import React from "react";
+import Rating from "./assets/Rating.png";
 
-const API_IMG='https://image.tmdb.org/t/p/w500/'
-const MovieBox =({titel,poster_path,vote_average,release_date,overview,popularity})=>{
-    return(
-        <>
-     
-        <div className='card text-center bg-s mb-3'>
-            <div className='card-body'>
-<img className='card-img-top' src={API_IMG+poster_path}/>
-            </div>
-<h1>Titel</h1>
+const API_IMG = "https://image.tmdb.org/t/p/w500/";
+const MovieBox = ({
+  titel,
+  poster_path,
+  vote_average,
+  release_date,
+  overview,
+  popularity,
+}) => {
+  return (
+    <>
+      <div className="col-lg-3">
+        <div className="card text-center bg-s mb-3">
+          <div className="card-body">
+            <img className="card-img-top" src={API_IMG + poster_path} />
+          </div>
+          <h1>Titel</h1>
 
-<img src={API_IMG+poster_path}></img>
-<img src={Rating} className="icon px-2 py-1" alt="Rating" />
-<p>{overview}</p>
-
+          <img src={API_IMG + poster_path}></img>
+          <img src={Rating} className="icon px-2 py-1" alt="Rating" />
+          <p>{overview}</p>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 export default MovieBox;
